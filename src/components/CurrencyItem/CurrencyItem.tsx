a9668496c8116e9987d77b4f1e2394b10d1af94c
Viktor Zhurbin <vzhurbin@gmail.com>
2022-03-10 16:48:57 +0300

    format price

--- src/components/CurrencyItem/CurrencyItem.tsx
+++ src/components/CurrencyItem/CurrencyItem.tsx
@@ -20,13 +20,20 @@ export const CurrencyItem = ({
   price,
   onClickRemove,
 }: CurrencyItemProps) => {
+  const formattedPrice = price
+    ? new Intl.NumberFormat("en", {
+        style: "currency",
+        currency: "EUR",
+      }).format(Number(price))
+    : "N/A";
+
   return (
     <Wrapper>
       <Item>
         <CupIcon />
         <Info>
           <Symbol>{symbol}</Symbol>
-          <Price>{price || "N/A"}</Price>
+          <Price>{formattedPrice}</Price>
         </Info>
       </Item>
       <RemoveIcon onClick={onClickRemove} />
