13383108e29a01d3fbb3ca3504b648993f74dabe
Viktor Zhurbin <vzhurbin@gmail.com>
2022-03-11 16:19:14 +0300

    refactor: cleanup

--- jest-setup.ts
+++
@@ -1,4 +0,0 @@
-import "@testing-library/jest-dom";
-
-// In jest.config.js add (if you haven't already)
-["<rootDir>/jest-setup.js"];
--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -11,16 +11,6 @@ export const GlobalStyle = createGlobalStyle`
         "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-
-        --color-white: #fff;
-        --color-black: rgba(0, 0, 0, 0.87);
-        --color-red: rgb(211, 47,47);
-        --color-bg: #2a0a4a;
-        --color-bg-grey: rgba(0, 0, 0, 0.12);
-        --color-bg-light-grey: rgba(0, 0, 0, 0.04);
-        --color-subtitle: #9484a4;
-        --color-grey: #bcbec2;
-        --color-orange: #fd4b24;
     }
 
     body, h1, h2 {
